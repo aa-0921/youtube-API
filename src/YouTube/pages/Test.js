9fd52aebfdca188777acc45b2f22480a04d4b095
akuto aoyama <aaaa.akuto@gmail.com>
2021-05-02 18:22:28 +0900

    [fix]Test.jsの内容追加

--- src/YouTube/pages/Test.js
+++ src/YouTube/pages/Test.js
@@ -4,15 +4,37 @@ import React, { useState, useEffect } from "react";
 
 export const Test = () => {
   const [textList, setTextList] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setTextList(["123", "456", "789"]);
+    const array = ["123", "456", "789"];
+    // const array = [
+    //   {
+    //     number: 123,
+    //     text: "いちにさん",
+    //   },
+    //   {
+    //     number: 456,
+    //     text: "よんごーろく",
+    //   },
+    //   {
+    //     number: 789,
+    //     text: "ななはちきゅー",
+    //   },
+    // ];
+    // setTextList(array);
+    setTextList(array);
+    // setItems(array);
   }, []);
 
   return (
     <div>
-      {textList.map((text) => (
-        <div>{text}</div>
+      {/* {textList.map((text) => ( */}
+      {items.map((item) => (
+        <div>
+          <div>{item.number}</div>
+          <div>{item.text}</div>
+        </div>
       ))}
     </div>
   );
